dfcf1fb074e3eeab75145f51eec9fe5f253adc46
hegusung <hegusung@gmail.com>
2022-10-21 01:04:14 +0200

    Config modification

--- redinfra.py
+++ redinfra.py
@@ -6,6 +6,7 @@ from routing import Routing
 
 def main():
     parser = argparse.ArgumentParser(description='RedInfra: red team infrastructure manager')
+    parser.add_argument('--show-config', action='store_true', help='Show the complete config', dest='show_config')
 
     aws_group = parser.add_argument_group("AWS")
     aws_group.add_argument('--list-aws', action='store_true', help='List AWS instances', dest='list_aws')
@@ -18,10 +19,16 @@ def main():
     aws_group.add_argument('--new-dns', nargs=2, metavar=("DNS", "IP"), type=str, help='Register new DNS', dest='new_dns')
     aws_group.add_argument('--remove-dns', nargs=2, metavar=("DNS", "IP"), type=str, help='Remove DNS entry', dest='remove_dns')
 
+    vpn_group = parser.add_argument_group("VPN")
+    vpn_group.add_argument('--set-vpn-ip', nargs=2, metavar=("Instance", "IP"), type=str, help='Set an Instance local VPN IP', dest='set_vpn_ip')
+    vpn_group.add_argument('--list-vpn-ip', action='store_true', help='List Instances VPN IP', dest='list_vpn_ip')
+    vpn_group.add_argument('--remove-vpn-ip', nargs=1, metavar=("Instance",), type=str, help='Remove Instance local VPN IP', dest='remove_vpn_ip')
+
     routing_group = parser.add_argument_group("Routing")
-    routing_group.add_argument('--set-vpn-ip', nargs=2, metavar=("Instance", "IP"), type=str, help='Set an Instance local VPN IP', dest='set_vpn_ip')
-    routing_group.add_argument('--list-vpn-ip', action='store_true', help='List Instances VPN IP', dest='list_vpn_ip')
-    routing_group.add_argument('--remove-vpn-ip', nargs=1, metavar=("Instance",), type=str, help='Remove Instance local VPN IP', dest='remove_vpn_ip')
+    routing_group.add_argument('--set-routing', nargs=2, metavar=("Instance", "IP"), type=str, help='Set a route between an Instance and an IP', dest='set_routing')
+    routing_group.add_argument('--list-routing', action='store_true', help='List routing', dest='list_routing')
+    routing_group.add_argument('--remove-routing', nargs=1, metavar=("Instance",), type=str, help='Remove a specific route', dest='remove_routing')
+
 
     args = parser.parse_args()
 
@@ -61,6 +68,43 @@ def main():
 
     if args.remove_vpn_ip:
         routing.remove_vpn_ip(args.remove_vpn_ip[0])
+
+    if args.set_routing:
+        routing.set_routing(args.set_routing[0], args.set_routing[1])
+
+    if args.list_routing:
+        routing.list_routing()
+
+    if args.remove_routing:
+        routing.remove_routing(args.remove_routing[0])
+
+
+    if args.show_config:
+        config = aws.show_config()
+        config = routing.show_config(config)
+
+        print("Current configuration:")
+        for c in config:
+            dns = c[0]
+            public_ip = c[1]
+            instance = c[2]
+            vpn_ip = c[3]
+            node_ip = c[4]
+
+            if dns != None:
+                dns = ("%s ===>   " % (dns,)).rjust(50)
+            else:
+                dns = " "*50
+
+            if instance != None:
+                row = dns + "[%s] %s [%s]" % (public_ip, instance, vpn_ip if vpn_ip != None else "")
+
+                if node_ip != None:
+                    row += "   <===>   %s" % node_ip
+            else:
+                row = dns + "[%s]" % public_ip
+
+            print(row)
 
 if __name__ == '__main__':
     main()
